8ebddbc39585c954de13a86e9ee0d07ea95d96f5
gaeun58 <gaeun58@naver.com>
2021-09-12 23:14:28 +0900

    삼행시) 버튼 클릭 시 각 운에 맞는 텍스트로 변경

---
+++ js/index.js
@@ -0,0 +1,12 @@
+function changeText1(){
+    let h1 = document.getElementsByTagName('h1');
+    h1[0].innerHTML="모든 사람들은"
+}
+function changeText2(){
+    let h1 = document.getElementsByTagName('h1');
+    h1[1].innerHTML="각자의 빛을 지니고 있어요"
+}
+function changeText3(){
+    let h1 = document.getElementsByTagName('h1');
+    h1[2].innerHTML="코딩하는 당신도 마찬가지! 화이팅!"
+}
