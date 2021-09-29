073f3d89028f5847f7df272ed087b1577a238cea
gaeun58 <gaeun58@naver.com>
2021-09-29 23:42:54 +0900

    캐릭터 이미지 추가

--- chatbot.js
+++ chatbot.js
@@ -2,12 +2,11 @@ function check_text() {
     let value = document.getElementById("console").value;
     console.log(value);
 
-    if(value === "안녕") {
-        document.getElementsByClassName('loopy_says')[0].innerHTML = '만나서 반가워!';
-
+    if(value === "배고프다") {
+        document.getElementsByClassName('loopy_says')[0].innerHTML = '난 오늘 로제떡볶이를 먹을거야!';
     }
-    else if(value === "불꺼줘") {
-        document.getElementsByClassName('loopy_says')[0].innerHTML = '벌써 자려구?';
+    else if(value === "졸리다. 불 끌게!") {
+        document.getElementsByClassName('loopy_says')[0].innerHTML = '그래 벌써 12시네~ 하암 잘자!';
         document.body.style.backgroundColor = 'gray';
     }
 }
